dedad335712c7aed3cb4bad6dea408e69ae94b9c
Mike McGrath <mike.mcgrath@rallysportdirect.com>
2020-01-02 13:50:59 -0700

    finish env logic

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,15 +7,11 @@ $packageRootPath = dirname(__DIR__);
 $packageRootEnv = $packageRootPath . '/.env';
 
 if (file_exists($rootEnv)) {
-	echo "YES1 \n\n";
-	print_r($rootEnv);
-	echo "\n\n";
 	$dotenv = Dotenv\Dotenv::createImmutable($rootPath);
 	$dotenv->load();
 } else if ($packageRootEnv . '/.env') {
-	echo "YES2 \n\n";
 	$dotenv = Dotenv\Dotenv::createImmutable($packageRootPath);
 	$dotenv->load();
 } else {
-	echo "YES3 \n\n";
+	throw new Exception('Cannot find an .env file');
 }
